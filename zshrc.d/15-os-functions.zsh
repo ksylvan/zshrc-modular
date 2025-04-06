f4c2cf0490eb4a0759c0a7bbe39fb5c0bf4c14a7
Kayvan Sylvan <kayvan@sylvan.com>
2025-04-06 16:06:26 -0700

    chore: enhance `_validated_hostname` with SSH and Tailscale checks
    ### CHANGES
    - Add SSH check before ping in hostname validation
    - Refine `.lan` suffix handling for ping with `.local`
    - Integrate Tailscale check for hostname resolution
    - Ensure fallback with Tailscale and ping if available
    - Improve hostname validation logic with added checks

--- zshrc.d/15-os-functions.zsh
+++ zshrc.d/15-os-functions.zsh
@@ -25,17 +25,35 @@ function _validated_hostname() {
         return 0
     fi
 
+    # Try ssh first
+    if ssh -o BatchMode=yes -o ConnectTimeout=5 "$host" exit &>/dev/null; then
+        echo "$host"
+        return 0
+    fi
+
     # Use ping, since it interacts well with mdns on macOS
     if ping -c 1 "$host" &>/dev/null; then
         echo "$host"
         return 0
     fi
+
     # Try changing the suffix to .local
     if [[ "$host" =~ \.lan$ ]]; then
-        host="${host%.lan}.local"
+        host="${host%.lan}"
     fi
-    if ping -c 1 "${host}" &>/dev/null; then
-        echo "${host}"
+    if ping -c 1 "${host}.local" &>/dev/null; then
+        echo "${host}.local"
+        return 0
+    fi
+
+    # Finally, if tailscale is installed and if tailscale is running
+    if command -v tailscale &>/dev/null && tailscale status | grep -q "$host"; then
+        host=$(tailscale status | grep "$host" | awk '{print $2}')
+        if [[ -n "$host" ]] && ping -c 1 "$host" &>/dev/null; then
+            echo "$host"
+            return 0
+        fi
+        echo "$host"
         return 0
     fi
     echo ""

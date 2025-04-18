be4c568582f4d608398600d56fca88cfc514d084
Kayvan Sylvan <kayvan@sylvan.com>
2025-04-17 17:07:13 -0700

    feat: add support for thefuck alias in zsh configuration
    - Introduce thefuck alias setup in zshrc.d directory.
    - Add conditional check for thefuck installation status.
    - Include verbose logging if thefuck is not installed.
    - Clean up by unsetting temporary verbose loading variable.

---
+++ zshrc.d/90-thefuck.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+eval $(thefuck --alias)
+
+_verbose_loading=${ZSHRC_VERBOSE:-0}
+
+if whence -p thefuck &>/dev/null; then
+    eval $(thefuck --alias)
+else
+    [[ ${_verbose_loading} -eq 1 ]] && echo "thefuck is not installed."
+fi
+
+unset _verbose_loading

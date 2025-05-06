149b3d9a9b0759fa9cae9747f1cf7caac673df04
Kayvan Sylvan <kayvan@sylvan.com>
2025-05-06 12:50:07 -0700

    chore: add plandex-cli (self-compiled go)

---
+++ zshrc.d/90-plandex.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+_verbose_loading=${ZSHRC_VERBOSE:-0}
+
+if whence -p plandex-cli &>/dev/null; then
+    alias plandex=plandex-cli
+    alias pdx=plandex-cli
+else
+    [[ ${_verbose_loading} -eq 1 ]] && echo "plandex-cli is not installed."
+fi
+
+unset _verbose_loading

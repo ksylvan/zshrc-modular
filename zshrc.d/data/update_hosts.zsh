f0c6d43d1995e5a13189e88226ee3bd2ee336423
Kayvan Sylvan <kayvan@sylvan.com>
2025-11-11 10:59:55 -0800

    chore: update host lists by removing 'kayvan-Meanwhile-MBP'
    ### CHANGES
    
    - Remove 'kayvan-Meanwhile-MBP' from MAC_BREW_HOSTS
    - Remove 'kayvan-Meanwhile-MBP' from ZSHRC_HOSTS
    - Remove 'kayvan-Meanwhile-MBP' from FABRIC_HOSTS

--- zshrc.d/data/update_hosts.zsh
+++ zshrc.d/data/update_hosts.zsh
@@ -1,7 +1,7 @@
 # Use _ZHU_ prefix to prevent naming conflicts
 _ZHU_WIN_HOSTS=(shakti)
-_ZHU_MAC_BREW_HOSTS=(dharma shiva zen kayvan-Meanwhile-MBP)
+_ZHU_MAC_BREW_HOSTS=(dharma shiva zen)
 _ZHU_MAC_APPSTORE_HOSTS=(dharma shiva zen)
 _ZHU_LINUX_HOSTS=(thor)
-_ZHU_ZSHRC_HOSTS=(dharma thor shiva zen kayvan-Meanwhile-MBP)
-_ZHU_FABRIC_HOSTS=(dharma thor shakti shiva zen kayvan-Meanwhile-MBP)
+_ZHU_ZSHRC_HOSTS=(dharma thor shiva zen)
+_ZHU_FABRIC_HOSTS=(dharma thor shakti shiva zen)

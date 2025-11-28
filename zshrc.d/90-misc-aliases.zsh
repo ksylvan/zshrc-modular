8d9687ccc6f021b229c80924823757760110afff
Kayvan Sylvan <kayvan@sylvan.com>
2025-11-29 04:22:02 +0800

    chore: remove `tailscale_restart` alias from misc-aliases
    ### CHANGES
    
    - Remove `tailscale_restart` alias for Tailscale restart
    - Retain `fabric_pr_count` alias for PR counting

--- zshrc.d/90-misc-aliases.zsh
+++ zshrc.d/90-misc-aliases.zsh
@@ -2,9 +2,6 @@
 
 # Miscellaneous aliases
 
-# Restart tailscale after a brew update
-alias tailscale_restart='sudo launchctl kickstart -k system/com.tailscale.tailscaled'
-
 # My PR count so far this month
 alias fabric_pr_count='python3 ~/src/backend-tools/count_merged_prs.py --username ksylvan --repo danielmiessler/fabric'
 

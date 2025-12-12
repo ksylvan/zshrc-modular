afcc05a8150f1ad9e3d17bffde6d5bbcfa9c7d80
Kayvan Sylvan <kayvan@sylvan.com>
2025-12-12 11:10:42 -0800

    chore: ensure zsh completion directory exists and update host sets
    - Create zsh completions directory before copying completion files
    - Update mac brew hosts list to remove zen machine
    - Update mac app store hosts list to remove zen machine
    - Add zen to linux hosts list for host updates
    - Keep zen in zshrc hosts while adjusting other groups
    - Preserve fabric hosts list while normalizing file newline ending

--- zshrc.d/data/update_hosts.zsh
+++ zshrc.d/data/update_hosts.zsh
@@ -1,7 +1,7 @@
 # Use _ZHU_ prefix to prevent naming conflicts
 _ZHU_WIN_HOSTS=(shakti)
-_ZHU_MAC_BREW_HOSTS=(dharma shiva zen)
-_ZHU_MAC_APPSTORE_HOSTS=(dharma shiva zen)
-_ZHU_LINUX_HOSTS=(thor)
+_ZHU_MAC_BREW_HOSTS=(dharma shiva)
+_ZHU_MAC_APPSTORE_HOSTS=(dharma shiva)
+_ZHU_LINUX_HOSTS=(thor zen)
 _ZHU_ZSHRC_HOSTS=(dharma thor shiva zen)
 _ZHU_FABRIC_HOSTS=(dharma thor shakti shiva zen)

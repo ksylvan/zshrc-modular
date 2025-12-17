02bcc4ddb04786ce9317d791d51405e41712073e
Kayvan Sylvan <kayvan@sylvan.com>
2025-12-17 15:06:12 -0800

    feat: add check for brew command before util-linux setup
    ### CHANGES
    
    - Add check for `brew` command availability
    - Ensure `util-linux` path setup only if `brew` exists
    - Maintain verbose loading messages for path updates
    - Unset `_zsh_util_linux_prefix` after use

--- zshrc.d/80-util-linux.zsh
+++ zshrc.d/80-util-linux.zsh
@@ -2,13 +2,15 @@
 
 _verbose_loading=${ZSHRC_VERBOSE:-0}
 
-if brew list | grep -q 'util-linux' &>/dev/null; then
-    [[ ${_verbose_loading} -eq 1 ]] && echo "util-linux is installed. Setting path..."
-    _zsh_util_linux_prefix="$(brew --prefix util-linux)"
-    if [[ -d "${_zsh_util_linux_prefix}/bin" ]]; then
-        [[ " ${path[@]} " != *" ${_zsh_util_linux_prefix}/bin "* ]] && path=("${_zsh_util_linux_prefix}/bin" $path)
-        [[ ${_verbose_loading} -eq 1 ]] && echo "Updated PATH: $PATH"
+if whence -p brew &>/dev/null; then
+    if brew list | grep -q 'util-linux' &>/dev/null; then
+        [[ ${_verbose_loading} -eq 1 ]] && echo "util-linux is installed. Setting path..."
+        _zsh_util_linux_prefix="$(brew --prefix util-linux)"
+        if [[ -d "${_zsh_util_linux_prefix}/bin" ]]; then
+            [[ " ${path[@]} " != *" ${_zsh_util_linux_prefix}/bin "* ]] && path=("${_zsh_util_linux_prefix}/bin" $path)
+            [[ ${_verbose_loading} -eq 1 ]] && echo "Updated PATH: $PATH"
+        fi
+        unset _zsh_util_linux_prefix
     fi
-    unset _zsh_util_linux_prefix
 fi
 true

470b5a51ab5d279eb26bb71b5d44490b3b47dcc8
Kayvan Sylvan <kayvan@vishnu.attlocal.net>
2026-03-23 16:11:28 -0700

    feat: add `vishnu` host to update target groups
    - Add `vishnu` to macOS Homebrew update host list.
    - Add `vishnu` to macOS App Store update hosts.
    - Include `vishnu` in Fabric and npm target hosts.
    - Add `vushnu` entry to zshrc update host list.

--- zshrc.d/data/update_hosts.zsh
+++ zshrc.d/data/update_hosts.zsh
@@ -1,8 +1,8 @@
 # Use _ZHU_ prefix to prevent naming conflicts
 _ZHU_WIN_HOSTS=(shakti)
-_ZHU_MAC_BREW_HOSTS=(dharma shiva)
-_ZHU_MAC_APPSTORE_HOSTS=(dharma shiva)
+_ZHU_MAC_BREW_HOSTS=(dharma shiva vishnu)
+_ZHU_MAC_APPSTORE_HOSTS=(dharma shiva vishnu)
 _ZHU_LINUX_HOSTS=(thor zen)
-_ZHU_ZSHRC_HOSTS=(dharma thor shiva zen)
-_ZHU_FABRIC_HOSTS=(dharma thor shakti shiva zen)
-_ZHU_NPM_HOSTS=(dharma shiva)
+_ZHU_ZSHRC_HOSTS=(dharma thor shiva vushnu zen)
+_ZHU_FABRIC_HOSTS=(dharma thor shakti shiva vishnu zen)
+_ZHU_NPM_HOSTS=(dharma shiva vishnu)
